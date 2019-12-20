e9a37ae1a5564fa01511a2cb06f09d2eab030896
Marc Foley <m.foley.88@gmail.com>
2019-12-20 16:42:50 +0000

    update build

--- sources/build.sh
+++ sources/build.sh
@@ -7,7 +7,7 @@ set -e
  fontmake -g MavenPro.glyphs -i -o ttf --output-dir ../fonts
  
  echo "Generating VFs"
- fontmake -g MavenPro.glyphs -o variable --output-path ../fonts/MavenPro-VF.ttf
+ fontmake -g MavenPro.glyphs -o variable --output-path ../fonts/MavenPro[wght].ttf
  
  rm -rf master_ufo/ instance_ufo/
  echo "Post processing"
@@ -17,27 +17,18 @@ ttfs=$(ls ../fonts/*.ttf)
 for ttf in $ttfs
 do
 	gftools fix-dsig -f $ttf;
-	./ttfautohint-vf $ttf "$ttf.fix";
+	gftools fix-nonhinting $ttf $ttf.fix;
 	mv "$ttf.fix" $ttf;
 done
 
-vfs=$(ls ../fonts/*-VF.ttf)
-for vf in $vfs
-do
-	gftools fix-dsig -f $vf;
-	./ttfautohint-vf --stem-width-mode nnn -x 0 $ttf "$ttf.fix";
-	mv "$vf.fix" $vf;
-done
-
+vfs=$(ls ../fonts/*\[wght\].ttf)
 gftools fix-vf-meta $vfs;
 for vf in $vfs
 do
+	gftools fix-unwanted-tables -t "MVAR" $vf
 	mv "$vf.fix" $vf;
-	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
-	rtrip=$(basename -s .ttf $vf)
-	new_file=../fonts/$rtrip.ttx;
-	rm $vf;
-	ttx $new_file
-	rm $new_file
+	gftools fix-dsig -f $vf;
+	gftools fix-nonhinting $vf $vf.fix;
+	mv "$vf.fix" $vf;
 done
-
+rm ../fonts/*gasp.ttf
